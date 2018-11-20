a3ea2ed54de3553476cca99f4c49be1ee45f6f06
Julieta Frade <up201506530@fe.up.pt>
2018-11-20 21:05:24 +0000

    Fixed route container size

--- grao/src/components/Route.vue
+++ grao/src/components/Route.vue
@@ -7,7 +7,11 @@
         <p> A1 </p>
         <div class="line"></div>
       </div>
-      <div class="products"></div>
+      <div class="products">
+        <p>Lorem ipsum dolor sit amet, an solum facilis assentior per, posse novum omittantur et pri. Aliquid atomorum dissentias mei ne. Nec ea enim admodum. Iriure efficiendi te est. Ea pro sale disputando, ei apeirian convenire vis. Ius et justo ludus, consul mucius no qui, te delicata voluptatum sed. Ei autem ullum vel.
+          Lorem ipsum dolor sit amet, an solum facilis assentior per, posse novum omittantur et pri. Aliquid atomorum dissentias mei ne. Nec ea enim admodum. Iriure efficiendi te est.
+        </p>
+       </div>
     </div>
 
     <div class=container>
@@ -16,7 +20,9 @@
         <p> A2 </p>
         <div class="line"></div>
       </div>
-      <div class="products"></div>
+      <div class="products">
+         <p>Lorem ipsum dolor sit amet, an solum facilis assentior per, posse novum omittantur et pri. Aliquid atomorum dissentias mei ne. Nec ea enim admodum. Iriure efficiendi te est. </p>
+      </div>
     </div>
 
     <div class=container>
@@ -25,7 +31,9 @@
         <p> A3 </p>
         <div class="line"></div>
       </div>
-      <div class="products"></div>
+      <div class="products">
+         <p>Lorem ipsum dolor sit amet, an solum facilis assentior per.</p>
+      </div>
     </div>
 
   </div>
@@ -33,62 +41,69 @@
 
 <script>
 export default {
-  name: 'Route',
-  props: {
-  }
-}
+  name: "Route",
+  props: {}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-div.container{
+div.container {
   width: 100%;
-  height: 15em;
-  display:flex;
+  /*height: 15em;*/
+  /*display:flex;*/
+  display: grid;
+  grid-template-columns: 6em auto;
+  grid-column-gap: 3em;
+  min-height: 4em;
 }
 
-div.left{
+div.left {
   display: flex;
   flex-direction: column;
   position: relative;
   height: 100%;
   width: 6em;
-  margin-right: 3em;
+  /*margin-right: 3em;*/
 }
 
-div.left .hexagon{
+div.left .hexagon {
   width: 100%;
 }
 
-div.left p{
+div.left p {
   position: absolute;
-  top:1em;
+  top: 1.1em;
   width: 100%;
   margin: 0;
   font-weight: lighter;
   text-align: center;
   color: var(--gold);
-  font-size:2.5em;
+  font-size: 2.5em;
 }
 
-div.line{
-  width: 51.5%;
-  border-right: 0.2em dotted var(--gold);
+div.line {
+  width: 52.5%;
+  border-right: 0.3em dotted var(--gold);
   height: 100%;
 }
 
-div.container:last-child div.line{
+div.container:last-child div.line {
   display: none;
 }
 
 div.products {
-  width: 100%;
+  /*width: 100%;*/
   background-color: var(--babyAlpha);
-  border-top: 0.2em solid var(--baby);
-  border-bottom: 0.2em solid var(--baby);
-  margin-top:2.6em;
- 
+  border-top: 0.1em solid var(--baby);
+  border-bottom: 0.1em solid var(--baby);
+  margin-top: 2.5em;
+  font-size:20px;
 }
 
+div.products p{
+  line-height: 1em;
+  padding-right: 1em;
+  padding-left: 1em;
+}
 </style>
