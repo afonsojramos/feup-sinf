cc7f6f06efc3abc046f508104abfd96d192a8a87
Afonso Ramos <up201506239@fe.up.pt>
2018-11-29 19:07:50 +0000

    App router views

--- grao/src/App.vue
+++ grao/src/App.vue
@@ -1,60 +1,52 @@
 <template>
   <div id="app">
-      <Navbar/>
-      <!--<Homepage/>-->
-      <!--<Orders msg="Client"/>-->
-      <Route/>
-      <Footer/>
+    <Navbar/>
+    <router-view/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
-//import Homepage from './components/Homepage.vue'
-//import Orders from './components/Orders.vue'
-import Route from './components/Route.vue'
 import Footer from './components/Footer.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     Navbar,
-    //Homepage,
-    //Orders,
-    Route,
     Footer
   }
 }
 </script>
-<style scoped>
 
-#app div:nth-child(2){
+<style>
+
+#app > div.navbar + div{
   padding-left: 20%;
   padding-right: 20%;
   margin-top:2em;
   padding-bottom: 2em;
 }
 
-
 /***MOBILE***/
 @media only screen and (max-width: 675px) {
 
-  #app div:nth-child(2){
+  #app > div.navbar + div{
       padding-left:2%;
   }
 
-  #app div:nth-child(2){
+  #app > div.navbar + div{
       padding-right:2%;
   }
 }
 
 @media only screen and (min-width: 675px) and (max-width: 1500px) {
 
-  #app div:nth-child(2){
+  #app > div.navbar + div{
       padding-left:10%;
   }
 
-  #app div:nth-child(2){
+  #app > div.navbar + div{
       padding-right:10%;
   }
 }
