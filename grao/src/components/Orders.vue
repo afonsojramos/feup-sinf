55f3f2a0a9e38c4471bdb52d75d6a9e89b6ab08b
Julieta Frade <up201506530@fe.up.pt>
2018-11-30 17:45:41 +0000

    Finished Acordeão

--- grao/src/components/Orders.vue
+++ grao/src/components/Orders.vue
@@ -2,14 +2,14 @@
   <div class="order">
     <h1> Client Orders </h1>
     <v-data-table v-model="selected"
-        :headers="headers"
+        :headers="ordersHeaders"
         :items="orders"
         select-all
         hide-actions
         class="elevation-0">
       <template slot="items" slot-scope="props">
         <tr>
-          <td><v-checkbox v-model="props.selected" primary hide-details></v-checkbox></td>
+          <td @click="handleCheckbox($event)"><v-checkbox v-model="props.selected" primary hide-details ></v-checkbox></td>
           <td class="text-xs-center" @click="expanded[props.item.id] = !expanded[props.item.id]">{{ props.item.number }}</td>
           <td class="text-xs-center" @click="expanded[props.item.id] = !expanded[props.item.id]">{{ props.item.id }}</td>
           <td class="text-xs-center" @click="expanded[props.item.id] = !expanded[props.item.id]">{{ props.item.client }}</td>
@@ -20,7 +20,18 @@
           <td colspan="100%">
             <v-expansion-panel>
               <v-expansion-panel-content v-model="expanded[props.item.id]">
-                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                    <v-data-table v-model="selected"
+                        :headers="productsHeaders"
+                        :items="props.item.products"
+                        hide-actions
+                        class="elevation-0 products">
+                      <template slot="items" slot-scope="props">
+                          <td class="text-xs-center">{{ props.item.name }}</td>
+                          <td class="text-xs-center">{{ props.item.section }}</td>
+                          <td class="text-xs-center">{{ props.item.qnt }}</td>
+                          <td class="text-xs-center">{{ props.item.stock }}</td>
+                      </template>
+                    </v-data-table>
               </v-expansion-panel-content>
             </v-expansion-panel>
           </td>
@@ -40,18 +51,46 @@ export default {
     return {
       expanded: {},
       selected: [],
-      headers: [
+      ordersHeaders: [
         { text: '#', align: 'center', value: 'number' },
         { text: 'id', align: 'center', value: 'id' },
         { text: 'Client', align: 'center', value: 'client' },
         { text: 'Request Date', align: 'center', value: 'request_date' },
         { text: 'Arrival Date', align: 'center', value: 'arrival_date' }
       ],
+      productsHeaders: [
+        { text: 'Product', align: 'center', value: 'name' },
+        { text: 'Section', align: 'center', value: 'section' },
+        { text: 'Qnt', align: 'center', value: 'qnt' },
+        { text: 'Stock', align: 'center', value: 'stock' },
+      ],
       orders: [
-        { value: false, number: 1, id: 'RDA21DA1', request_date: '01-03-2018', arrival_date: '25-04-2018', client: 'Lorem' },
-        { value: false, number: 2, id: 'JRGE0YK4', request_date: '06-03-2018', arrival_date: '27-04-2018', client: 'Ipsum' },
-        { value: false, number: 3, id: 'P1EASD31', request_date: '03-03-2018', arrival_date: '26-04-2018', client: 'Dolor' },
-        { value: false, number: 4, id: 'TSDFS123', request_date: '02-03-2018', arrival_date: '25-04-2018', client: 'Sit' }
+        { value: false, number: 1, id: 'RDA21DA1', request_date: '01-03-2018', arrival_date: '25-04-2018', client: 'Lorem',
+          products: [
+            { name: 'Lorem', qnt: 3, stock: 4, section:"A"},
+            { name: 'Ipsum', qnt: 4, stock: 5, section:"B"},
+            { name: 'Dolor', qnt: 3, stock: 6, section:"C"},
+            { name: 'Sit', qnt: 4, stock: 8, section:"D"}
+          ]
+        },
+        { value: false, number: 2, id: 'JRGE0YK4', request_date: '06-03-2018', arrival_date: '27-04-2018', client: 'Ipsum', 
+          products: [
+            { name: 'Ipsum', qnt: 4, stock: 5, section : "B"},
+            { name: 'Dolor', qnt: 3, stock: 6, section: "C"},
+            { name: 'Sit', qnt: 4, stock: 10, section : "D"}
+          ]
+        },
+        { value: false, number: 3, id: 'P1EASD31', request_date: '03-03-2018', arrival_date: '26-04-2018', client: 'Dolor',
+          products: [
+            { name: 'Dolor', qnt: 3, stock: 9, section: "C"},
+            { name: 'Sit', qnt: 4, stock: 8, section : "D"}
+          ]
+        },
+        { value: false, number: 4, id: 'TSDFS123', request_date: '02-03-2018', arrival_date: '25-04-2018', client: 'Sit',
+          products: [
+            { name: 'Ipsum', qnt: 4, stock: 9, section : "B"}
+          ]
+        }
       ]
     }
   },
@@ -61,7 +100,21 @@ export default {
       this.$set(this.expanded, i.id, false)
     })
     // console.log(this.expanded)
+  },
+
+  methods:{
+    handleCheckbox: function (event){
+      
+      var tr = event.target.closest("tr");
+      if(tr.getAttribute("active")=="true"){
+        tr.removeAttribute("active");
+      }    
+      else{
+        tr.setAttribute("active", "true"); 
+      }
+    }
   }
+  
 }
 </script>
 <style scoped>
@@ -70,8 +123,13 @@ h1{
   margin-bottom: 1em;
 }
 
-tbody tr{
-  color: var(--light) !important;
+tr.expand td{
+  background-color: transparent !important;
+  padding:0 !important;
+}
+
+div.products >>>tbody{
+  background-color: transparent !important;
 }
 
 ul{
