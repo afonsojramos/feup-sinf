4327c3fac0d7acfe010c565f590be837fb2f59b1
Afonso Ramos <up201506239@fe.up.pt>
2018-11-30 18:21:57 +0000

    Linted code

--- grao/src/components/Orders.vue
+++ grao/src/components/Orders.vue
@@ -62,33 +62,57 @@ export default {
         { text: 'Product', align: 'center', value: 'name' },
         { text: 'Section', align: 'center', value: 'section' },
         { text: 'Qnt', align: 'center', value: 'qnt' },
-        { text: 'Stock', align: 'center', value: 'stock' },
+        { text: 'Stock', align: 'center', value: 'stock' }
       ],
       orders: [
-        { value: false, number: 1, id: 'RDA21DA1', request_date: '01-03-2018', arrival_date: '25-04-2018', client: 'Lorem',
+        {
+          value: false,
+          number: 1,
+          id: 'RDA21DA1',
+          request_date: '01-03-2018',
+          arrival_date: '25-04-2018',
+          client: 'Lorem',
           products: [
-            { name: 'Lorem', qnt: 3, stock: 4, section:"A"},
-            { name: 'Ipsum', qnt: 4, stock: 5, section:"B"},
-            { name: 'Dolor', qnt: 3, stock: 6, section:"C"},
-            { name: 'Sit', qnt: 4, stock: 8, section:"D"}
+            { name: 'Lorem', qnt: 3, stock: 4, section: 'A' },
+            { name: 'Ipsum', qnt: 4, stock: 5, section: 'B' },
+            { name: 'Dolor', qnt: 3, stock: 6, section: 'C' },
+            { name: 'Sit', qnt: 4, stock: 8, section: 'D' }
           ]
         },
-        { value: false, number: 2, id: 'JRGE0YK4', request_date: '06-03-2018', arrival_date: '27-04-2018', client: 'Ipsum', 
+        {
+          value: false,
+          number: 2,
+          id: 'JRGE0YK4',
+          request_date: '06-03-2018',
+          arrival_date: '27-04-2018',
+          client: 'Ipsum',
           products: [
-            { name: 'Ipsum', qnt: 4, stock: 5, section : "B"},
-            { name: 'Dolor', qnt: 3, stock: 6, section: "C"},
-            { name: 'Sit', qnt: 4, stock: 10, section : "D"}
+            { name: 'Ipsum', qnt: 4, stock: 5, section: 'B' },
+            { name: 'Dolor', qnt: 3, stock: 6, section: 'C' },
+            { name: 'Sit', qnt: 4, stock: 10, section: 'D' }
           ]
         },
-        { value: false, number: 3, id: 'P1EASD31', request_date: '03-03-2018', arrival_date: '26-04-2018', client: 'Dolor',
+        {
+          value: false,
+          number: 3,
+          id: 'P1EASD31',
+          request_date: '03-03-2018',
+          arrival_date: '26-04-2018',
+          client: 'Dolor',
           products: [
-            { name: 'Dolor', qnt: 3, stock: 9, section: "C"},
-            { name: 'Sit', qnt: 4, stock: 8, section : "D"}
+            { name: 'Dolor', qnt: 3, stock: 9, section: 'C' },
+            { name: 'Sit', qnt: 4, stock: 8, section: 'D' }
           ]
         },
-        { value: false, number: 4, id: 'TSDFS123', request_date: '02-03-2018', arrival_date: '25-04-2018', client: 'Sit',
+        {
+          value: false,
+          number: 4,
+          id: 'TSDFS123',
+          request_date: '02-03-2018',
+          arrival_date: '25-04-2018',
+          client: 'Sit',
           products: [
-            { name: 'Ipsum', qnt: 4, stock: 9, section : "B"}
+            { name: 'Ipsum', qnt: 4, stock: 9, section: 'B' }
           ]
         }
       ]
@@ -102,19 +126,17 @@ export default {
     // console.log(this.expanded)
   },
 
-  methods:{
-    handleCheckbox: function (event){
-      
-      var tr = event.target.closest("tr");
-      if(tr.getAttribute("active")=="true"){
-        tr.removeAttribute("active");
-      }    
-      else{
-        tr.setAttribute("active", "true"); 
+  methods: {
+    handleCheckbox: function (event) {
+      var tr = event.target.closest('tr')
+      if (tr.getAttribute('active') === 'true') {
+        tr.removeAttribute('active')
+      } else {
+        tr.setAttribute('active', 'true')
       }
     }
   }
-  
+
 }
 </script>
 <style scoped>
